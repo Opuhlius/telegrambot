222bfc9beb9c082c61f66f8baa660788f7fd1cec
AiorNerok <90855034+AiorNerok@users.noreply.github.com>
2023-01-14 11:55:54 +0300

    Update utills.py

--- utills.py
+++ utills.py
@@ -8,6 +8,7 @@ class ConvertionExeption(Exception):
 class CryptoConverter:
     @staticmethod
     def convert(quote: str, base: str, amount: str):
+        
         if quote == base:
             raise ConvertionExeption(f'Невозможно перевести одинаковые валюты {base}.')
 
@@ -15,22 +16,19 @@ class CryptoConverter:
             quote_ticker = keys[quote]
         except KeyError:
             raise ConvertionExeption(f'Не удалось обработать валюту {quote}')
-
         try:
             base_ticker = keys[base]
         except KeyError:
             raise ConvertionExeption(f'Не удалось обработать валюту {base}')
-      
+
         try: 
             amount = float(amount)
         except ValueError:
             raise ConvertionExeption(f'Не удалось обработать валюту {amount}')
 
     
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker} \
-            &api_key={"903d325cd8aa165d9d51e09bc94ff2f1fff1401b06bed64ba2d17a1953aa5beb"}')
-        total_base = json.loads(r.content)[keys[base]]
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}&api_key={"903d325cd8aa165d9d51e09bc94ff2f1fff1401b06bed64ba2d17a1953aa5beb"}')
+
+        total_base = json.loads(r.content)[base_ticker]
 
         return total_base
-
-
