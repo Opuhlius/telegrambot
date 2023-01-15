0b316e98c6297c3a27c60460886dc14b57d4610e
Полина <guf.fiska@gmail.com>
2023-01-15 20:38:13 +0300

    new file

--- App.py
+++ App.py
@@ -7,21 +7,21 @@ bot = telebot.TeleBot(TOKEN)
 
 
 
-@bot.message_handler(commands=['start''help'])
+@bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
     text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
     <в какую валюту перевести> \
     <количество переводимой валюты>\nУвидеть список всех доступных валют: /values'
-    bot.reply_to(message, text)
+    bot.send_message(message.chat.id, text) 
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
         text = '\n'.join((text, key, ))
-    bot.reply_to(message, text)
+    bot.send_message(message.chat.id, text)
 
-@bot.message_handler(commands_types=['text'])
+@bot.message_handler(content_types=['text'])
 def convert(message: telebot.types.Message):
     try:
         values = message.text.split(' ')
@@ -31,10 +31,12 @@ def convert(message: telebot.types.Message):
 
         quote, base, amount = values
         total_base = CryptoConverter.convert(quote, base, amount)
+    
     except ConvertionExeption as e:
-        bot.reply_to(message, f'Ошибка пользователя.\n{e}')     
+        bot.send_message(message.chat.id, f'Ошибка пользователя.\n{e}')     
+    
     except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+        bot.send_message(message.chat.id, f'Не удалось обработать команду\n{e}')
     else:
         text = f'Цена {amount} {quote} в {base} - {total_base}'
         bot.send_message(message.chat.id, text)
